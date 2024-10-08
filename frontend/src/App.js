6401209081bf2d4321a36a3f1bc2a17b6b87f552
Clene <104350902+Clene1@users.noreply.github.com>
2024-10-08 21:32:46 +0200

    temp page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Temp from './pages/temp';
 
 
 function App() {
-    return (
-        <div className="App">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </div>
-    );
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  };
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Temp />} /> 
+        <Route path="/login" element={<Login onFormSwitch={toggleForm} />} />
+        <Route path="/register" element={<Register onFormSwitch={toggleForm} />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
