23d842b5e5f7901370893ea916ef95cd74256737
ST10219213 <moolmans20013@gmail.com>
2024-10-08 16:19:41 +0200

    bruteforce preventions

--- backend/routes/payments.js
+++ backend/routes/payments.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const {createPayment, getPayment, getPayments} = require('../Controllers/PaymentController')
+const { createPayment, getPayment, getPayments } = require('../Controllers/PaymentController');
+const ExpressBrute = require('express-brute');
+const store = new ExpressBrute.MemoryStore();
+
+const bruteforcePayment = new ExpressBrute(store, {
+    freeRetries: 5,
+    minWait: 1000 * 60,
+    maxWait: 1000 * 60 * 10,
+    lifetime: 1000 * 60 * 10,
+});
 
 const router = express.Router();
 
 router.get('/', getPayments);
-router.post('/', createPayment);
+router.post('/', bruteforcePayment.prevent, createPayment);
 router.get('/:id', getPayment);
 
 module.exports = router;
